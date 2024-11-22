d3189e6638a270e750cb9e74deb154b69c05ce04
elbio <webstands@gmail.com>
2024-11-22 16:38:31 -0300

    creating theme and adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import { Button } from '@mui/material';
+import { Box, ThemeProvider, Typography } from '@mui/material';
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography variant="h3" component="h1">
+        Home
+      </Typography>
+    </Box>
+  );
+};
+
+const About = () => {
+  return (
+    <Box>
+      <Typography variant="h3" component="h1">
+       About
+      </Typography>
+    </Box>
+  );
+};
 
 function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return <ThemeProvider theme={appTheme}>
+    <Box component="main"
+      sx={{
+        height: "100vh",
+        backgroundColor: (theme) => theme.palette.grey[900],
+      }}
+    
+     >
+       <Header></Header>
+       <Layout>
+         <Routes>
+           <Route path="/" element={< Home />} />
+           <Route path="about" element={< About />} />
+         </Routes>
+       </Layout>
+    </Box>
+  </ThemeProvider>;
 }
 
 export default App;
